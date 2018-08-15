7633da3571f2ac30f7d90b17713a3d196c096bc4
Marcin Czekala <marcin_czekala@icloud.com>
2018-08-15 19:37:58 +0200

    add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://serduch:password1@ds113452.mlab.com:13452/mydatabase");
+mongoose.connect("mongodb: //serduch:password1@ds141221.mlab.com:41221/database-1");
 
 const userSchema = new Schema({
     name: String,
